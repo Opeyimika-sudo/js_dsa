0728ea38fce34b0b00fb81987ac2751c58320349
Opeyimika Aremu <opeyimikaaremu@gmail.com>
2024-01-26 18:35:06 +0100

    tower of hanoi solution

--- towerOfHanoi.js
+++ towerOfHanoi.js
@@ -1,1 +1,9 @@
- 
+function towersOfHanoi(n, A, B,C){
+    if(n> 0){
+        towersOfHanoi(n-1, A, C, B)
+        console.log(`${A} -> ${C}`)
+        towersOfHanoi(n-1, B, A, C)
+    }
+}
+
+console.log(towersOfHanoi(3, 1, 2, 3))
